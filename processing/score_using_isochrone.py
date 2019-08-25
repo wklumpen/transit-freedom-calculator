119a66b53b800ee450717b19d9e71daedc77beb8
Willem Klumpenhouwer <willemjjk@gmail.com>
2019-08-24 22:40:51 -0400

    Updated coding

--- processing/score_using_isochrone.py
+++ processing/score_using_isochrone.py
@@ -2,15 +2,23 @@ import pandas as pd
 import urllib.request
 import urllib.parse
 from shapely.geometry import shape, GeometryCollection, Point, MultiPolygon
+from bs4 import BeautifulSoup
 import json
 import pandas as pd
 
 city = 'calgary'
 workspace = 'data'
 
-centroids = pd.read_csv('data\calgary_da_centroids.csv')
-# Get the isochrone for a given GPS point
+centroids = pd.read_csv(f'data\\{city}_rtm_centroids.csv')
 
+supermarket = []
+with open(f"C:\\OTP\\graphs\\{city}\\poi\\supermarket.osm") as infile:
+    data = BeautifulSoup(infile, 'lxml')
+    for node in data.find_all('node'):
+        supermarket.append([float(node['lon']), float(node['lat'])])
+
+# Get the isochrone for a given GPS point
+result_list = []
 # Go through each centroid
 count = 0
 for oix, origin in centroids.iterrows():
@@ -28,15 +36,41 @@ for oix, origin in centroids.iterrows():
     # Fetch the isochrone
     qs = urllib.parse.urlencode(params)
     url = f"http://localhost:8080/otp/routers/{city}/isochrone?{qs}"
-    print(f"{count}. Loading isochrone for {origin.DAUID}")
-    with urllib.request.urlopen(url) as u:
-        data = u.read()
-        isochrone = json.loads(data)
-        g = GeometryCollection([shape(feature["geometry"]) for feature in isochrone['features']]).geoms[0]
-        inside = 0
-        for dix, dest in centroids.iterrows():
-            p = Point(dest.X,dest.Y)
-            if g.contains(p):
-                inside += 1
-        print(f"You can reach {inside} centroids from {origin.DAUID}")
+    header = {'Accept': 'application/json'}
+    req = urllib.request.Request(url, headers=header)
+    print(f"{count}. Loading isochrone for {origin.tz}")
+    print(url)
+    try:
+        with urllib.request.urlopen(req) as u:
+            data = u.read()
+            isochrone = json.loads(data)
+            g = GeometryCollection([shape(feature["geometry"]) for feature in isochrone['features']]).geoms[0]
+            inside = 0
+            pop = 0
+            jobs = 0
+            supermarkets = 0
+            for dix, dest in centroids.iterrows():
+                p = Point(dest.X,dest.Y)
+                if g.contains(p):
+                    inside += 1
+                    # Now we score it with the data we have
+
+                    # First we add population
+                    pop += dest.pop2014
+                    jobs += dest.jobs2014
+                    for s in supermarket:
+                        p = Point(s[0], s[1])
+                        if g.contains(p):
+                            supermarkets += 1
+
+            result = [origin.tz, pop, jobs, supermarkets]
+            result_list.append(result)
+            print(result)
+            print(f"You can reach {inside} centroids from {origin.tz}")
+            print(f"You can reach {supermarkets} supermarkets from {origin.tz}")
+    except urllib.error.HTTPError:
+        print(f"ERROR: {url}")
+
+out = pd.DataFrame(result_list, columns=['orig', 'dest', 'pop', 'jobs', 'supermarket'])
+out.to_csv(f'od_{city}.csv', index=False)
         
