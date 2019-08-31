039e521644c236052fb915b786b2f1045d5d5479
Willem Klumpenhouwer <willemjjk@gmail.com>
2019-08-31 18:20:47 -0400

    Updated to not use museums.

--- calgary/score_calgary.py
+++ calgary/score_calgary.py
@@ -2,10 +2,12 @@ import pandas as pd
 import urllib.request
 import urllib.parse
 from shapely.geometry import shape, GeometryCollection, Point, MultiPolygon
+from shapely.errors import TopologicalError
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
 import os
+import numpy as np
 
 # CURRENTLY CALGARY FOCUSED
 workspace = 'data'
@@ -44,18 +46,29 @@ for oix, origin in centroids.iterrows():
         with urllib.request.urlopen(req) as u:
             data = u.read()
             isochrone = json.loads(data)
-            g = GeometryCollection([shape(feature["geometry"]) for feature in isochrone['features']]).geoms[0].buffer(0)
+            # g = GeometryCollection([shape(feature["geometry"]) for feature in isochrone['features']]).geoms[0].buffer(0)
+            g = shape(isochrone["features"][0]['geometry'])
+            print(f"Valid geometry: {g.is_valid}")
+            print(f"Area: {g.area}")
+            area = g.area
+            # if not g.is_valid:
+            #     g = g.buffer(0)
             inside = 0
-
             # Add up population and employment based on overlapping centroids
             pop = 0
             jobs = 0
             for dix, dest in centroids.iterrows():
                 p = Point(dest.X,dest.Y)
-                if p.within(g):
-                    inside += 1
-                    pop += dest.pop2014
-                    jobs += dest.jobs2014
+                try:
+                    if p.within(g):
+                        inside += 1
+                        pop += dest.pop2014
+                        jobs += dest.jobs2014
+                except TopologicalError:
+                    g = g.buffer(0)
+                    new_area = g.area
+                    print(f"Invalid Geometry, buffer applied. New area: {new_area}")
+                    errors.append([origin.tz, area, new_area])
 
             # Iterate through POI geometry and add to the collection
             supermarkets = 0
@@ -77,8 +90,9 @@ for oix, origin in centroids.iterrows():
             for feature in poi_json['features']:
                 # First create the point from the geometry and see if it's in the isochrone
                 point = shape(feature['geometry'])
+                # print(point)
                 poi = feature['properties']
-                if g.contains(point):
+                if point.within(g):
                     # Now we check based on ameity
                     if "shop" in poi and poi['shop'] == 'supermarket':
                         supermarkets += 1
@@ -87,7 +101,7 @@ for oix, origin in centroids.iterrows():
                     elif "amenity" in poi and poi['amenity'] == 'restaurant':
                         restaurants += 1
                     elif "leisure" in poi and poi['leisure'] == 'playground':
-                        pubs += 1
+                        playgrounds += 1
                     elif "amenity" in poi and poi['amenity'] == 'school':
                         schools += 1
                     elif "amenity" in poi and poi['amenity'] == 'childcare':
@@ -100,8 +114,6 @@ for oix, origin in centroids.iterrows():
                         theatres += 1
                     elif "amenity" in poi and poi['amenity'] == 'cinema':
                         cinemas += 1
-                    elif "amenity" in poi and poi['amenity'] == 'museum':
-                        museums += 1
                     elif "amenity" in poi and poi['amenity'] == 'pharmacy':
                         pharmacies += 1
                     elif "amenity" in poi and (poi['amenity'] == 'clinic' or poi['amenity'] == 'hospital'):
@@ -109,7 +121,7 @@ for oix, origin in centroids.iterrows():
                     elif "amenity" in poi and poi['amenity'] == 'dentist':
                         dentists += 1
 
-            result = [origin.tz, pop, jobs, supermarkets, restaurants, pubs, playgrounds, schools, childcares, parks, fitnesses, theatres, cinemas, museums, pharmacies, clinics, dentists]
+            result = [origin.tz, pop, jobs, supermarkets, restaurants, pubs, playgrounds, schools, childcares, parks, fitnesses, theatres, cinemas, pharmacies, clinics, dentists]
             result_list.append(result)
             print(result)
             print(f"You can reach {inside} centroids from {origin.tz}")
@@ -118,36 +130,68 @@ for oix, origin in centroids.iterrows():
         print(f"ERROR: {url}")
         errors.append(origin.tz)
 
-print(errors)
-out = pd.DataFrame(result_list, columns=['orig', 'population', 'jobs', 'supermarket', 'restaurant', 'pub', 'playground', 'school', 'childcare', 'park', 'fitness', 'theatre', 'cinema', 'museum', 'pharmacy', 'clinic', 'dentist'])
+out = pd.DataFrame(result_list, columns=['orig', 'population', 'jobs', 'supermarket', 'restaurant', 'pub', 'playground', 'school', 'childcare', 'park', 'fitness', 'theatre', 'cinema', 'pharmacy', 'clinic', 'dentist'])
+
 
 out['pop_pctl'] = out.population.rank(pct=True)
 out['job_pctl'] = out.jobs.rank(pct=True)
 out['supermarket_pctl'] = out.supermarket.rank(pct=True)
 out['basics_avg'] = (out.pop_pctl + out.job_pctl + out.supermarket_pctl)/3
+col = 'basics_avg'
+conditions  = [ out[col] < 0.2, out[col] < 0.4, out[col] < 0.6, out[col] < 0.8, out[col] < 0.9, out[col] >=0.9 ]
+choices     = [ "F", 'D', 'C', "B", "A", "A+"]
+out['basics_grade'] = np.select(conditions, choices, default=np.nan)
 
 out['restaurant_pctl'] = out.restaurant.rank(pct=True)
 out['pub_pctl'] = out.pub.rank(pct=True)
 out['food_avg'] = (out.restaurant_pctl + out.pub_pctl)/2
+col = 'food_avg'
+conditions  = [ out[col] < 0.2, out[col] < 0.4, out[col] < 0.6, out[col] < 0.8, out[col] < 0.9, out[col] >=0.9 ]
+choices     = [ "F", 'D', 'C', "B", "A", "A+"]
+out['food_grade'] = np.select(conditions, choices, default=np.nan)
 
 out['playground_pctl'] = out.playground.rank(pct=True)
 out['school_pctl'] = out.school.rank(pct=True)
 out['childcare_pctl'] = out.childcare.rank(pct=True)
 out['family_avg'] = (out.playground_pctl + out.school_pctl + out.childcare_pctl)/3
+col = 'family_avg'
+conditions  = [ out[col] < 0.2, out[col] < 0.4, out[col] < 0.6, out[col] < 0.8, out[col] < 0.9, out[col] >=0.9 ]
+choices     = [ "F", 'D', 'C', "B", "A", "A+"]
+out['family_grade'] = np.select(conditions, choices, default=np.nan)
 
 out['park_pctl'] = out.park.rank(pct=True)
 out['fitness_pctl'] = out.fitness.rank(pct=True)
 out['recreation_avg'] = (out.park_pctl + out.fitness_pctl)/2
+col = 'recreation_avg'
+conditions  = [ out[col] < 0.2, out[col] < 0.4, out[col] < 0.6, out[col] < 0.8, out[col] < 0.9, out[col] >=0.9 ]
+choices     = [ "F", 'D', 'C', "B", "A", "A+"]
+out['recreation_grade'] = np.select(conditions, choices, default=np.nan)
 
 out['theatre_pctl'] = out.theatre.rank(pct=True)
 out['cinema_pctl'] = out.cinema.rank(pct=True)
-out['musuem_pctl'] = out.museum.rank(pct=True)
-out['arts_avg'] = (out.theatre_pctl + out.cinema_pctl + out.musuem_pctl)/3
+out['arts_avg'] = (out.theatre_pctl + out.cinema_pctl)/2
+col = 'arts_avg'
+conditions  = [ out[col] < 0.2, out[col] < 0.4, out[col] < 0.6, out[col] < 0.8, out[col] < 0.9, out[col] >=0.9 ]
+choices     = [ "F", 'D', 'C', "B", "A", "A+"]
+out['arts_grade'] = np.select(conditions, choices, default=np.nan)
 
 out['pharmacy_pctl'] = out.pharmacy.rank(pct=True)
 out['clinic_pctl'] = out.clinic.rank(pct=True)
 out['dentist_pctl'] = out.dentist.rank(pct=True)
 out['health_avg'] = (out.pharmacy_pctl + out.clinic_pctl + out.dentist_pctl)/3
+col = 'health_avg'
+conditions  = [ out[col] < 0.2, out[col] < 0.4, out[col] < 0.6, out[col] < 0.8, out[col] < 0.9, out[col] >=0.9 ]
+choices     = [ "F", 'D', 'C', "B", "A", "A+"]
+out['health_grade'] = np.select(conditions, choices, default=np.nan)
+
+out['total_avg'] = (out.basics_avg + (out.family_avg + out.food_avg + out.recreation_avg + out.arts_avg + out.health_avg)/5)/2
+col = 'total_avg'
+conditions  = [ out[col] < 0.2, out[col] < 0.4, out[col] < 0.6, out[col] < 0.8, out[col] < 0.9, out[col] >=0.9 ]
+choices     = [ "F", 'D', 'C', "B", "A", "A+"]
+out['total_grade'] = np.select(conditions, choices, default=np.nan)
+
+out = out[(out.jobs > 10) & (out.population > 10)]
 
 out.to_csv(f'tfc_calgary.csv', index=False)
-        
+
+print("Errors:", errors)
